d82cf748daab72c4ae4c79255cfbb07ae00fe5fb
tim <t.p.maerov@tinkoff.ru>
2022-03-25 19:31:53 +0300

    добавил Spring

---
+++ src/main/kotlin/example/pavel/Person.kt
@@ -0,0 +1,19 @@
+package example.pavel
+
+class Person(
+    val name: String,
+    val age: Int,
+    val car: List<Car>,
+    val education: Education,
+    val mail: String,
+    val address: Map<String, String>,
+    val tel: String
+)
+
+class Car(
+    val type: String,
+    val number: String
+)
+
+enum class Education { Basic, Middle, Magister }
+
